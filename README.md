5642dd95501a95930f2d4b8f16a73ac3483ad343
darshan rathod <darshan.rathod@zeuslearning.com>
2023-11-09 14:00:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Task1-ZL (React + TypeScript + Vite)
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./App.css"
+
+export default function App(): React.ReactNode {
+
+	return (
+		<main>
+			Task 1
+		</main>
+	);
+}
